cd459965846a96ce14ff58917b07fab973842caf
ZachDaChampion <zachchampion79@gmail.com>
2018-07-23 18:27:01 -0400

    added motor sync groups

---
+++ include/motor_sync_group.h
@@ -0,0 +1,39 @@
+#ifndef MOTOR_SYNC_H_
+#define MOTOR_SYNC_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <sensor.h>
+
+// group of motors to sync
+// like drivestraight
+class MotorSyncGroup {
+
+public:
+
+  // enable or disable group
+  bool enabled;
+
+  // pointer to array of motors in this group
+  int *motors;
+
+  // amount of motors in group
+  int motor_count;
+
+  // recalculate the mean motor position
+  void updateData();
+
+private:
+
+  // mean motor position
+  float mean_position;
+
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
