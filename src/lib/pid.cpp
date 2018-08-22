aa56ac1754737400b5a18e3cbdd2484becb32f82
Zach Champion <zachchampion79@gmail.com>
2018-08-22 09:39:51 -0400

    actually use velocity calculations in D

--- src/lib/pid.cpp
+++ src/lib/pid.cpp
@@ -29,15 +29,12 @@ float Pid :: update(float current, float given_vel, float time_delta) {
   // calculate error
   float error = target - current;
 
-  // calculate previous error (assuming same target)
-  float prev_error = target - prev_pos;
-
   // if unspecified, calculate velocity
-  float vel = (given_vel || given_vel == 0) ? given_vel : current - prev_pos;
+  float vel = (given_vel || given_vel == 0) ? given_vel : (current - prev_pos) / time_delta;
 
   // calculate P, and D
   float p = error * kp;
-  float d = (prev_error - error) * kd / time_delta;
+  float d = -vel * kd / time_delta;
 
   // integrate if necessary
   if (anti_windup_threshold == 0 || fabs(vel) < anti_windup_threshold) integral += error / time_delta;
